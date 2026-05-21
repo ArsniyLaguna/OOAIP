eac183e929f7cbfbbc15a413ef69aefb97abc88d
Arseni <logunov7v@gamil.com>
2026-05-21 21:31:44 +0600

    fix: explicit namespace for IoC and force add all files

--- SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
+++ SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
@@ -6,10 +6,10 @@ public class RegisterIoCDependencyMoveCommand : ICommand
 {
     public void Execute()
     {
-        IoC.Register("Commands.Move", args =>
+        SpaceBattle.Lib.IoC.Register("Commands.Move", args =>
         {
             var obj = args[0];
-            var movableObject = IoC.Resolve<IMovable>("Adapters.IMovingObject", obj);
+            var movableObject = SpaceBattle.Lib.IoC.Resolve<IMovable>("Adapters.IMovingObject", obj);
             return new MoveCommand(movableObject);
         });
     }
