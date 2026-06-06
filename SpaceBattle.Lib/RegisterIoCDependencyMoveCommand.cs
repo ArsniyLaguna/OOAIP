2aa49002d4b2ec9e481001051ddbb7dd449f9872
kkksarrste <emmptisstery@gmail.com>
2026-06-06 20:48:30 +0600

    Update RegisterIoCDependencyMoveCommand.cs

--- SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
+++ SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
@@ -7,8 +7,8 @@ namespace SpaceBattle.Lib
             IoC.Register("Commands.Move", (args) =>
             {
                 var obj = args[0];
-                var movable = IoC.Resolve<IMovable>("Adapters.IMovable", obj);
-                return new MoveCommand(movable);
+                var movingObject = IoC.Resolve<IMovingObject>("Adapters.IMovingObject", obj);
+                return new MoveCommand(movingObject);
             });
         }
     }
