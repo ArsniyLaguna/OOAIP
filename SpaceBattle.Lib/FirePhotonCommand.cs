d2618596165ddcaf4a7dc08107c01a8c5a5851ba
VladimirChalov <vladimircalov557@gmail.com>
2026-06-06 13:10:39 +0600

    Update FirePhotonCommand.cs

--- SpaceBattle.Lib/FirePhotonCommand.cs
+++ SpaceBattle.Lib/FirePhotonCommand.cs
@@ -1,21 +1,11 @@
 namespace SpaceBattle.Lib;
 
-/// <summary>
-/// Команда для выстрела фотонной торпеды
-/// Применяется DIP - команда зависит от интерфейсов, а не от конкретных классов
-/// </summary>
 public class FirePhotonCommand : ICommand
 {
     private readonly Spaceship _spaceship;
     private readonly (int X, int Y) _direction;
     private readonly IGameObjectRepository _repository;
 
-    /// <summary>
-    /// Создать команду выстрела
-    /// </summary>
-    /// <param name="spaceship">Корабль, который стреляет</param>
-    /// <param name="direction">Направление выстрела</param>
-    /// <param name="repository">Репозиторий для добавления торпеды</param>
     public FirePhotonCommand(Spaceship spaceship, (int X, int Y) direction, IGameObjectRepository repository)
     {
         _spaceship = spaceship ?? throw new ArgumentNullException(nameof(spaceship));
@@ -25,10 +15,8 @@ public class FirePhotonCommand : ICommand
 
     public void Execute()
     {
-        // Создаем фотонную торпеду
         var photon = _spaceship.FirePhoton(_direction);
-
-        // Добавляем её в репозиторий
+        
         _repository.Add(photon);
     }
 }
