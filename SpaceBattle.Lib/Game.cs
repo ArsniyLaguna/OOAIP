e86239b20472aa11e5735b351471765890269cc5
VladimirChalov <vladimircalov557@gmail.com>
2026-06-06 13:05:04 +0600

    Update Game.cs

--- SpaceBattle.Lib/Game.cs
+++ SpaceBattle.Lib/Game.cs
@@ -29,7 +29,7 @@ public class Game
             {
                 command.Execute();
             }
-            catch (Exception _)
+            catch (Exception)
             {
                 
             }
