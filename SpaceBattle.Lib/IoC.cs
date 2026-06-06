7a69b3bedd58da01c31529ce273ff2dcca7293eb
kkksarrste <emmptisstery@gmail.com>
2026-06-06 20:27:53 +0600

    Update IoC.cs

--- SpaceBattle.Lib/IoC.cs
+++ SpaceBattle.Lib/IoC.cs
@@ -3,22 +3,6 @@ using System.Collections.Generic;
 
 namespace SpaceBattle.Lib
 {
-    private readonly static Dictionary<string, Func<object[], object>> _strategies = new();
-
-    public static T Resolve<T>(string key, params object[] args)
-    {
-        if (_strategies.TryGetValue(key, out var strategy))
-        {
-            return (T)strategy(args);
-        }
-        throw new Exception($"Зависимость {key} не зарегистрирована.");
-    }
-
-    public static void Register(string key, Func<object[], object> strategy)
-    {
-        _strategies[key] = strategy;
-    private static readonly Dictionary<string, IStrategy> _strategies = new();
-    public static T Resolve<T>(string key, params object[] args)
     public static class IoC
     {
         // Используем один общий словарь для всех стратегий
