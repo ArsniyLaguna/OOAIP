8969f6d81e57d24a35ba1d33af5c6f25c51974b5
VladimirChalov <vladimircalov557@gmail.com>
2026-06-06 16:16:09 +0600

    Update IGameObject.cs

--- SpaceBattle.Lib/IGameObject.cs
+++ SpaceBattle.Lib/IGameObject.cs
@@ -3,8 +3,11 @@ namespace SpaceBattle.Lib;
 public interface IGameObject
 {
     int Id { get; }
+}
 
+public interface IMovable : IGameObject
+{
     (int X, int Y) Position { get; set; }
-
+    (int X, int Y) Velocity { get; }
     void Update();
 }
