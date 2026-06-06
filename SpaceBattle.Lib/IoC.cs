0afbe5fb4a43e99b3940a7906e49006f8ed98d58
kkksarrste <emmptisstery@gmail.com>
2026-06-06 19:36:54 +0600

    Update IoC.cs

--- SpaceBattle.Lib/IoC.cs
+++ SpaceBattle.Lib/IoC.cs
@@ -22,43 +22,5 @@ namespace SpaceBattle.Lib
         }
 
         public static void Reset() => _strategies.Clear();
-{
-public static class IoC
-{
-    private static readonly Dictionary<string, Func<object[], object>> _strategies = new();
-
-    static IoC()
-    {
-        // Упрощаем регистрацию: передаем просто ключ и делегат
-        _strategies["IoC.Register"] = (args) =>
-        {
-            var key = (string)args[0];
-            // Здесь мы ожидаем делегат. Если передается мок, он сломается.
-            var strategy = (Func<object[], object>)args[1];
-            _strategies[key] = strategy;
-            return null!; 
-        };
     }
-
-    public static T Resolve<T>(string key, params object[] args)
-    {
-        // Специальная обработка для IoC.Register, чтобы не искать в словаре, 
-        // если стратегия еще не добавлена
-        if (key == "IoC.Register")
-        {
-            return (T)_strategies["IoC.Register"](args);
-        }
-
-        if (_strategies.TryGetValue(key, out var strategy))
-        {
-            return (T)strategy(args);
-        }
-        throw new Exception($"Зависимость {key} не зарегистрирована.");
-    }
-
-    public static void Register(string key, Func<object[], object> strategy)
-    {
-        _strategies[key] = strategy;
-    }
-}
 }
