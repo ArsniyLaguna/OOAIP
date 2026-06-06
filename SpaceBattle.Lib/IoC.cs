56441ab14301520a4ff57dfa05dc5f6d83579ab1
kkksarrste <emmptisstery@gmail.com>
2026-06-06 20:14:35 +0600

    Refactor IoC.cs to improve code structure

--- SpaceBattle.Lib/IoC.cs
+++ SpaceBattle.Lib/IoC.cs
@@ -2,13 +2,13 @@ using System;
 using System.Collections.Generic;
 
 namespace SpaceBattle.Lib
-{
+{ // Уровень 1: Namespace
     public static class IoC
-    {
+    { // Уровень 2: Класс
         private static readonly Dictionary<string, Func<object[], object>> _registry = new();
 
         public static void Register(string key, Func<object[], object> strategy)
-        {
+        { // Уровень 3: Методы
             _registry[key] = strategy;
         }
 
@@ -22,5 +22,5 @@ namespace SpaceBattle.Lib
         }
 
         public static void Reset() => _registry.Clear();
-    }
-}
+    } // Закрытие уровня 2 (Класс)
+} // Закрытие уровня 1 (Namespace)
