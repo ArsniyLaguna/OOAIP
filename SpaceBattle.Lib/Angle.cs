a2937da340b741c0d82651f22b96de690a85f097
kkksarrste <emmptisstery@gmail.com>
2026-06-06 19:36:37 +0600

    Update Angle.cs

--- SpaceBattle.Lib/Angle.cs
+++ SpaceBattle.Lib/Angle.cs
@@ -4,7 +4,42 @@ namespace SpaceBattle.Lib
 {
     public class Angle 
     { 
-        public int Numerator { get; } 
-        public Angle(int n) => Numerator = n; 
+        public static int Denominator { get; set; } = 8;
+        public int Numerator { get; }
+
+        public Angle(int numerator)
+        {
+            Numerator = ((numerator % Denominator) + Denominator) % Denominator;
+        }
+
+        public static Angle operator +(Angle a, Angle b)
+        {
+            return new Angle(a.Numerator + b.Numerator);
+        }
+
+        public static implicit operator double(Angle angle)
+        {
+            return (double)angle.Numerator / Denominator * 2 * Math.PI;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Angle objectAngle && Numerator == objectAngle.Numerator;
+        }
+
+        public override int GetHashCode()
+        {
+            return Numerator.GetHashCode();
+        }
+
+        public static bool operator ==(Angle? a, Angle? b)
+        {
+            return ReferenceEquals(a, b) || (a is not null && a.Equals(b));
+        }
+
+        public static bool operator !=(Angle? a, Angle? b)
+        {
+            return !(a == b);
+        }
     }
 }
