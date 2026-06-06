c55ffb3aa22c333e035a75a470956f8eaa467422
VladimirChalov <vladimircalov557@gmail.com>
2026-06-06 19:54:26 +0600

    Update

--- SpaceBattle.Lib/IoC.cs
+++ SpaceBattle.Lib/IoC.cs
@@ -1,30 +1,26 @@
-namespace SpaceBattle.Lib;
+using System;
+using System.Collections.Generic;
 
-public static class IoC
+namespace SpaceBattle.Lib
 {
-    private static readonly Dictionary<string, Func<object[], object>> _registry = new();
+    public static class IoC
+    {
+        private static readonly Dictionary<string, Func<object[], object>> _registry = new();
 
-    public static void Register(string key, Func<object[], object> strategy)
-    {
-        _registry[key] = strategy;
-    }
+        public static void Register(string key, Func<object[], object> strategy)
+        {
+            _registry[key] = strategy;
+        }
 
-    public static T Resolve<T>(string key, params object[] args)
-    {
-        if (_registry.TryGetValue(key, out var strategy))
+        public static T Resolve<T>(string key, params object[] args)
         {
-            return (T)strategy(args);
+            if (_registry.TryGetValue(key, out var strategy))
+            {
+                return (T)strategy(args);
+            }
+            throw new Exception($"Зависимость '{key}' не найдена.");
         }
-        throw new Exception($"Зависимость '{key}' не найдена.");
-    private static readonly Dictionary<string, IStrategy> _strategies = new();
-    public static T Resolve<T>(string key, params object[] args)
-    {
-        if (key == "IoC.Register")
-        {
-            _strategies[(string)args[0]] = (IStrategy)args[1];
-            return default!;
-        }
-        if (_strategies.TryGetValue(key, out var strategy)) return (T)strategy.Invoke(args);
-        throw new Exception($"Зависимость {key} не найдена.");
+
+        public static void Reset() => _registry.Clear();
     }
 }
