2e156b70103c7a83124480878933641aec5f4d06
VladimirChalov <vladimircalov557@gmail.com>
2026-06-06 13:09:13 +0600

    Update PhotonMovementCommand.cs

--- SpaceBattle.Lib/PhotonMovementCommand.cs
+++ SpaceBattle.Lib/PhotonMovementCommand.cs
@@ -4,10 +4,6 @@ public class PhotonMovementCommand : ICommand
 {
     private readonly Photon _photon;
 
-    /// <summary>
-    /// Создать команду движения фотона
-    /// </summary>
-    /// <param name="photon">Фотон для перемещения</param>
     public PhotonMovementCommand(Photon photon)
     {
         _photon = photon ?? throw new ArgumentNullException(nameof(photon));
