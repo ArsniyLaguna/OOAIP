6b4ba50ee904b0bdddba1310e4309b7ea9fb5168
kkksarrste <emmptisstery@gmail.com>
2026-06-06 19:39:57 +0600

    Update Angle.cs

--- SpaceBattle.Lib/Angle.cs
+++ SpaceBattle.Lib/Angle.cs
@@ -2,57 +2,8 @@ using System;
 
 namespace SpaceBattle.Lib
 {
-    public static int Denominator { get; set; } = 8;
-    public int Numerator { get; }
-
-    public Angle(int numerator)
+    public class Angle
     {
-        int maxUnits = Denominator; 
-        int resolved = numerator % maxUnits;
-        if (resolved < 0) resolved += maxUnits;
-        Numerator = resolved;
-        Numerator = ((numerator % Denominator) + Denominator) % Denominator;
-    }
-
-    public static Angle operator +(Angle a, Angle b)
-    {
-        return new Angle(a.Numerator + b.Numerator);
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is Angle other) return this.Numerator == other.Numerator;
-        return false;
-    }
-
-    public override int GetHashCode() => Numerator.GetHashCode();
-    public static implicit operator double(Angle angle)
-    {
-        return (double)angle.Numerator / Denominator * 2 * Math.PI;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        var objectAngle = obj as Angle;
-
-        return objectAngle is not null && Numerator == objectAngle.Numerator;
-    }
-
-    public override int GetHashCode()
-    {
-        return Numerator.GetHashCode();
-    }
-
-    public static bool operator ==(Angle? a, Angle? b)
-    {
-        return ReferenceEquals(a, b) || (a is not null && a.Equals(b));
-    }
-
-    public static bool operator !=(Angle? a, Angle? b)
-    {
-        return !(a == b);
-    public class Angle 
-    { 
         public static int Denominator { get; set; } = 8;
         public int Numerator { get; }
 
@@ -66,29 +17,19 @@ namespace SpaceBattle.Lib
             return new Angle(a.Numerator + b.Numerator);
         }
 
+        public override bool Equals(object? obj)
+        {
+            return obj is Angle other && Numerator == other.Numerator;
+        }
+
+        public override int GetHashCode() => Numerator.GetHashCode();
+
         public static implicit operator double(Angle angle)
         {
             return (double)angle.Numerator / Denominator * 2 * Math.PI;
         }
 
-        public override bool Equals(object? obj)
-        {
-            return obj is Angle objectAngle && Numerator == objectAngle.Numerator;
-        }
-
-        public override int GetHashCode()
-        {
-            return Numerator.GetHashCode();
-        }
-
-        public static bool operator ==(Angle? a, Angle? b)
-        {
-            return ReferenceEquals(a, b) || (a is not null && a.Equals(b));
-        }
-
-        public static bool operator !=(Angle? a, Angle? b)
-        {
-            return !(a == b);
-        }
+        public static bool operator ==(Angle? a, Angle? b) => ReferenceEquals(a, b) || (a is not null && a.Equals(b));
+        public static bool operator !=(Angle? a, Angle? b) => !(a == b);
     }
 }
