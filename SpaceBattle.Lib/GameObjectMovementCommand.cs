49d55cf981135bdd53cc1863d4d0886a0da6d013
kkksarrste <emmptisstery@gmail.com>
2026-06-06 22:35:18 +0600

    Update GameObjectMovementCommand.cs

--- SpaceBattle.Lib/GameObjectMovementCommand.cs
+++ SpaceBattle.Lib/GameObjectMovementCommand.cs
@@ -1,17 +1,19 @@
-public class GameObjectMovementCommand : ICommand
-{
-    private readonly IMovable _gameObject;
-    
-    public GameObjectMovementCommand(IMovable gameObject)
-    private readonly IMovable _movable;
-    
-    public GameObjectMovementCommand(IMovable movable)
-    {
-        _movable = movable ?? throw new ArgumentNullException(nameof(movable));
-    }
+using System;
 
-    public void Execute()
+namespace SpaceBattle.Lib
+{
+    public class GameObjectMovementCommand : ICommand
     {
-        _movable.Update();
+        private readonly IMovable _movable;
+        
+        public GameObjectMovementCommand(IMovable movable)
+        {
+            _movable = movable ?? throw new ArgumentNullException(nameof(movable));
+        }
+
+        public void Execute()
+        {
+            _movable.Update();
+        }
     }
 }
