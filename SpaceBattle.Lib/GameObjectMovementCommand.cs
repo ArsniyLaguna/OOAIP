107c00d17797f616f145b8d51f21fb200d2fb5cc
VladimirChalov <vladimircalov557@gmail.com>
2026-06-06 13:08:39 +0600

    Update GameObjectMovementCommand.cs

--- SpaceBattle.Lib/GameObjectMovementCommand.cs
+++ SpaceBattle.Lib/GameObjectMovementCommand.cs
@@ -3,11 +3,7 @@ namespace SpaceBattle.Lib;
 public class GameObjectMovementCommand : ICommand
 {
     private readonly IGameObject _gameObject;
-
-    /// <summary>
-    /// Создать команду движения игрового объекта
-    /// </summary>
-    /// <param name="gameObject">Игровой объект для перемещения</param>
+    
     public GameObjectMovementCommand(IGameObject gameObject)
     {
         _gameObject = gameObject ?? throw new ArgumentNullException(nameof(gameObject));
