006bc328dc2fc28ffe292d2a24bd0dad86c9adb2
kkksarrste <emmptisstery@gmail.com>
2026-06-06 19:21:34 +0600

    Update CreateMacroCommandStrategy.cs

--- SpaceBattle.Lib/CreateMacroCommandStrategy.cs
+++ SpaceBattle.Lib/CreateMacroCommandStrategy.cs
@@ -11,8 +11,13 @@ public class CreateMacroCommandStrategy : IStrategy
 
     public object Invoke(params object[] args)
     {
+        // 1. Получаем список имен команд
         var commandNames = IoC.Resolve<IEnumerable<string>>(_commandSpec, args);
+        
+        // 2. Создаем команды
         var commands = commandNames.Select(name => IoC.Resolve<ICommand>(name, args));
+        
+        // 3. Возвращаем макрос
         return new MacroCommand(commands);
     }
 }
