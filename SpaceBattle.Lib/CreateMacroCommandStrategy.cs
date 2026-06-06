fad5e74ca6d702f415cf83241b2372629e9d7d14
kkksarrste <emmptisstery@gmail.com>
2026-06-06 19:16:04 +0600

    Enhance Invoke method with comments for clarity
    Added comments to clarify the steps for invoking macro commands.

--- SpaceBattle.Lib/CreateMacroCommandStrategy.cs
+++ SpaceBattle.Lib/CreateMacroCommandStrategy.cs
@@ -11,16 +11,13 @@ public class CreateMacroCommandStrategy : IStrategy
 
     public object Invoke(params object[] args)
     {
+        // 1. Получаем список имен команд, из которых состоит макрос
         var commandNames = IoC.Resolve<IEnumerable<string>>(_commandSpec);
 
+        // 2. Создаем экземпляры всех этих команд, передавая аргументы (например, игровой объект)
         var commands = commandNames.Select(name => IoC.Resolve<ICommand>(name, args));
-        }
 
-    public CreateMacroCommandStrategy(string commandSpec) => _commandSpec = commandSpec;
-    public object Invoke(params object[] args)
-    {
-        var commandNames = IoC.Resolve<IEnumerable<string>>(_commandSpec);
-        var commands = commandNames.Select(name => IoC.Resolve<ICommand>(name, args));
+        // 3. Возвращаем готовую макро-команду
         return new MacroCommand(commands);
     }
 }
