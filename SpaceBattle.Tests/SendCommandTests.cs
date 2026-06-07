be58d18531fb4a4f37b17e383f23b7394bfac6ca
Arseni <logunov7v@gamil.com>
2026-06-07 16:39:14 +0600

    fix: Add IoC.Reset() to SendCommand and Rotate tests
    - Fix failing tests due to IoC container state pollution
    - Reset IoC before each test to ensure clean state
    - Fixes 'Зависимость не найдена' errors

--- SpaceBattle.Tests/SendCommandTests.cs
+++ SpaceBattle.Tests/SendCommandTests.cs
@@ -10,47 +10,40 @@ public class SendCommandTests
     [Fact]
     public void SendCommand_ValidArgs_CallsReceiveWithCorrectCommand()
     {
-        // Arrange
         var receiverMock = new Mock<ICommandReceiver>();
         var internalCommandMock = new Mock<ICommand>();
         var sendCommand = new SendCommand(internalCommandMock.Object, receiverMock.Object);
 
-        // Act
         sendCommand.Execute();
 
-        // Assert
         receiverMock.Verify(r => r.Receive(internalCommandMock.Object), Times.Once);
     }
 
     [Fact]
     public void SendCommand_ReceiverThrows_ThrowsException()
     {
-        // Arrange
         var receiverMock = new Mock<ICommandReceiver>();
         var internalCommandMock = new Mock<ICommand>();
-        
+
         receiverMock.Setup(r => r.Receive(It.IsAny<ICommand>())).Throws(new Exception());
         var sendCommand = new SendCommand(internalCommandMock.Object, receiverMock.Object);
 
-        // Act & Assert
         Assert.Throws<Exception>(() => sendCommand.Execute());
     }
 
     [Fact]
     public void RegisterSendCommandDependency_Execute_ResolvesCorrectly()
     {
-        // Arrange
+        IoC.Reset();
+
         var receiverMock = new Mock<ICommandReceiver>();
         var internalCommandMock = new Mock<ICommand>();
 
-        // 1. Вызываем команду регистрации зависимости
         ICommand registerCmd = new RegisterIoCDependencySendCommand();
         registerCmd.Execute();
 
-        // 2. Разрешаем зависимость Commands.Send через IoC
         var resolvedCommand = IoC.Resolve<ICommand>("Commands.Send", internalCommandMock.Object, receiverMock.Object);
 
-        // 3. Проверяем критерии приемки
         Assert.NotNull(resolvedCommand);
         Assert.IsType<SendCommand>(resolvedCommand);
     }
