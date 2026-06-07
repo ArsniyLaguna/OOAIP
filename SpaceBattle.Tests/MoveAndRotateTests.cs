c146504aca3e11b69027d674285ae7ba082fc4ad
kkksarrste <emmptisstery@gmail.com>
2026-06-07 08:01:11 +0600

    Update MoveAndRotateTests.cs

--- SpaceBattle.Tests/MoveAndRotateTests.cs
+++ SpaceBattle.Tests/MoveAndRotateTests.cs
@@ -6,19 +6,21 @@ namespace SpaceBattle.Tests;
 
 public class MoveAndRotateTests
 {
-    [Fact]
-    public void RegisterMoveCommand_ResolvesCorrectly()
-    {
-        var movingObjectMock = new Mock<IMovingObject>();
-        var dummyGameObject = new object();
+[Fact]
+public void RegisterMoveCommand_ResolvesCorrectly()
+{
+    // 1. Создаем мок объекта, который умеет двигаться
+    var movingObjectMock = new Mock<IMovingObject>();
 
-        IoC.Register("Adapters.IMovingObject", (args) => movingObjectMock.Object);
+    // 2. Регистрируем команду
+    ICommand registerCmd = new RegisterIoCDependencyMoveCommand();
+    registerCmd.Execute();
 
-        ICommand registerCmd = new RegisterIoCDependencyMoveCommand();
-        registerCmd.Execute();
-        var resolvedCommand = IoC.Resolve<ICommand>("Commands.Move", dummyGameObject);
+    // 3. Вызываем Resolve, передавая сам мок-объект в качестве аргумента
+    var resolvedCommand = IoC.Resolve<ICommand>("Commands.Move", movingObjectMock.Object);
 
-        Assert.NotNull(resolvedCommand);
-        Assert.IsType<MoveCommand>(resolvedCommand);
-    }
+    // 4. Проверяем
+    Assert.NotNull(resolvedCommand);
+    Assert.IsType<MoveCommand>(resolvedCommand);
+}
 }
