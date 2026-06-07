ae281b90aa612493b40cd9d7fbb15042e8a6bbe1
VladimirChalov <vladimircalov557@gmail.com>
2026-06-07 11:27:56 +0600

    Update RegisterIoCRotateTests.cs

--- SpaceBattle.Tests/RegisterIoCRotateTests.cs
+++ SpaceBattle.Tests/RegisterIoCRotateTests.cs
@@ -5,32 +5,33 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 
-
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
 
-namespace SpaceBattle.Tests;
-
-public class RegisterIoCRotateTests
+namespace SpaceBattle.Tests
 {
- [Fact]
-public void RegisterIoCDependencyRotateCommand_RegistersAndResolvesCorrectly()
-{
-    // 1. Очистка всегда в самом начале
-    IoC.Reset();
-    
-    // 2. Сначала регистрируем ВСЕ зависимости
-    IoC.Register("Adapters.IRotatable", args => new Mock<IRotatable>().Object);
-    
-    // 3. Регистрируем команду
-    new RegisterIoCDependencyRotateCommand().Execute();
-    
-    // 4. Резолвим
-    var resolvedCommand = IoC.Resolve<ICommand>("Commands.Rotate", new Mock<object>().Object);
-    
-    // 5. Проверка
-    Assert.NotNull(resolvedCommand);
-    Assert.IsType<RotateCommand>(resolvedCommand);
-    
-    // 6. Очистка после теста, чтобы не мешать другим (хорошая практика)
-    IoC.Reset();
+    public class RegisterIoCRotateTests
+    {
+        [Fact]
+        public void RegisterIoCDependencyRotateCommand_RegistersAndResolvesCorrectly()
+        {
+            // 1. Очистка всегда в самом начале
+            IoC.Reset();
+            
+            // 2. Сначала регистрируем ВСЕ зависимости
+            IoC.Register("Adapters.IRotatable", args => new Mock<IRotatable>().Object);
+            
+            // 3. Регистрируем команду
+            new RegisterIoCDependencyRotateCommand().Execute();
+            
+            // 4. Резолвим
+            var resolvedCommand = IoC.Resolve<ICommand>("Commands.Rotate", new Mock<object>().Object);
+            
+            // 5. Проверка
+            Assert.NotNull(resolvedCommand);
+            Assert.IsType<RotateCommand>(resolvedCommand);
+            
+            // 6. Очистка после теста
+            IoC.Reset();
+        }
+    }
 }
