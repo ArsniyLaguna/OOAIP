fa19d615832d743945a47354a963c532b0742ed2
VladimirChalov <vladimircalov557@gmail.com>
2026-06-07 11:40:44 +0600

    Update

--- SpaceBattle.Lib/PhotonMovementCommand.cs
+++ SpaceBattle.Lib/PhotonMovementCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using SpaceBattle.Lib;
 
 namespace SpaceBattle.Lib
@@ -6,9 +7,15 @@ namespace SpaceBattle.Lib
     {
         private readonly IMovable _movable;
 
+        /// <summary>
+        /// Команда для перемещения объекта, реализующего IMovable.
+        /// </summary>
+        /// <param name="movable">Объект для перемещения.</param>
+        /// <exception cref="ArgumentNullException">Выбрасывается, если movable равен null.</exception>
         public PhotonMovementCommand(IMovable movable)
         {
-            _movable = movable;
+            // Используем оператор объединения с null для инициализации и выброса исключения
+            _movable = movable ?? throw new ArgumentNullException(nameof(movable));
         }
 
         public void Execute()
