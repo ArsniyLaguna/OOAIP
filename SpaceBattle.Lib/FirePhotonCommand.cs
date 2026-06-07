9d9422e062a638461af1ad4b7c573e99103fab85
kkksarrste <emmptisstery@gmail.com>
2026-06-07 09:17:25 +0600

    Update FirePhotonCommand.cs

--- SpaceBattle.Lib/FirePhotonCommand.cs
+++ SpaceBattle.Lib/FirePhotonCommand.cs
@@ -1,16 +1,11 @@
+using System;
+using SpaceBattle.Lib;
+
 namespace SpaceBattle.Lib;
 
 public class FirePhotonCommand : ICommand
 {
     private readonly Spaceship _spaceship;
-    private readonly (int X, int Y) _direction;
-
-    public FirePhotonCommand(
-        Spaceship spaceship, 
-        (int X, int Y) direction)
-    {
-        _spaceship = spaceship ?? throw new ArgumentNullException(nameof(spaceship));
-        _direction = direction;
     private readonly Vector _direction;
     private readonly IGameObjectRepository _repository;
 
