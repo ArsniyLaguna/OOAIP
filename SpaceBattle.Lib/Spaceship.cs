6df5edd935a74dae8f26f26039cee4a57ee2c338
kkksarrste <emmptisstery@gmail.com>
2026-06-07 09:18:38 +0600

    Update Spaceship.cs

--- SpaceBattle.Lib/Spaceship.cs
+++ SpaceBattle.Lib/Spaceship.cs
@@ -1,36 +1,38 @@
-namespace SpaceBattle.Lib;
+using System;
+using SpaceBattle.Lib;
 
-public class Spaceship : IMovable
+namespace SpaceBattle.Lib
 {
-    private int _nextPhotonId = 1;
-    private Vector _velocity;
-
-    public int Id { get; }
-    public Vector Position { get; set; }
-    public Vector Velocity => _velocity;
-
-    public Spaceship(int id, Vector position)
+    public class Spaceship : IMovable
     {
-        Id = id;
-        Position = position;
-        _velocity = new Vector(0, 0);
-    }
+        private int _nextPhotonId = 1;
+        private Vector _velocity;
 
-    public void SetVelocity(Vector velocity)
-    {
-    }
+        public int Id { get; }
+        public Vector Position { get; set; }
+        public Vector Velocity => _velocity;
 
-    public IGameObject FirePhoton((int X, int Y) direction)
-        _velocity = velocity;
-    }
+        public Spaceship(int id, Vector position)
+        {
+            Id = id;
+            Position = position;
+            _velocity = new Vector(0, 0);
+        }
 
-    public void Update()
-    {
-        Position = new Vector(Position.X + _velocity.X, Position.Y + _velocity.Y);
-    }
+        public void SetVelocity(Vector velocity)
+        {
+            _velocity = velocity;
+        }
 
-    public Photon FirePhoton(Vector direction)
-    {
-        return new Photon(_nextPhotonId++, Position, direction);
+        public void Update()
+        {
+            Position = new Vector(Position.X + _velocity.X, Position.Y + _velocity.Y);
+        }
+
+        // Исправлено: теперь метод принимает Vector, соответствующий сигнатуре в Photon
+        public Photon FirePhoton(Vector direction)
+        {
+            return new Photon(_nextPhotonId++, Position, direction);
+        }
     }
 }
